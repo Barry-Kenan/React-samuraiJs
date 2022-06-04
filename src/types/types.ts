55b574066b106f2aaeee23708f9d5edcdc89a229
Barry-Kenan <bary.berdiyev.97@mail.ru>
2022-06-04 19:19:57 +0700

    added new files

---
+++ src/types/types.ts
@@ -0,0 +1,28 @@
+export type PostType = {
+    id: number
+    message: string
+    likesCount: number
+}
+export type ContactsType = {
+    github: string
+    vk: string
+    facebook: string
+    instagram: string
+    twitter: string
+    website: string
+    youtube: string
+    mainLink: string
+}
+export type PhotosType = {
+    small: string | null
+    large: string | null
+}
+
+export type UsersType = {
+    id: number
+    name: string
+    status: string
+    photos: PhotosType
+    followed:boolean
+
+}
